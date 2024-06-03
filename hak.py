bb8065fd94484c6830fa664db30db7e94d1394c0
Goran Školjarev <gskoljarev@gmail.com>
2024-06-03 10:37:03 +0200

    Modify link in HAK email

--- hak.py
+++ hak.py
@@ -162,8 +162,8 @@ def process(url):
         )
         subject = f'[{COMPANY_NAME}] {unit_label}'
         body = f'<!DOCTYPE html><html><body><p>HAK - Pomorski promet {date_raw}</p><br>'\
-            '<a href="https://www.hak.hr/info/stanje-na-cestama/#trajektni-promet">'\
-            'https://www.hak.hr/info/stanje-na-cestama/#trajektni-promet</a></body></html>'.strip()
+            '<a href="https://m.hak.hr/stanje.asp?id=3">'\
+            'https://m.hak.hr/stanje.asp?id=3/a></body></html>'.strip()
 
         # retrieve islands connected to this unit
         islands = next(
